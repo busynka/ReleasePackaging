e6b2604287e6def7c41460b7aa9826f6ab797100
Hanna Vainshtein <anuta.ann@gmail.com>
2015-12-30 14:35:48 -0500

    commit

--- release_packaging.groovy
+++ release_packaging.groovy
@@ -4,11 +4,16 @@ import java.nio.channels.FileChannel
 import java.util.zip.ZipFile
 import java.util.Enumeration
 
-static void main(args){
-if (args.size()<2) {println "directory source_file conf_file"}
-else {zip(args[0], args[1], args[2])}
+static void main (args){
+	if (args.size() < 2) {
+		println "directory source_file conf_file"
+	}
+	else {
+	zip (args[0], args[1], args[2])
+	}
 }
-static void zip(String dir, String source_file, String conf_file){
+
+static void zip ( String dir, String source_file, String conf_file  ){
     def ant = new AntBuilder()
     def path = dir + source_file
     def conf_path = dir + conf_file
@@ -29,63 +34,69 @@ static void zip(String dir, String source_file, String conf_file){
     
     //reading zip file
     
-    Enumeration<? extends ZipEntry> e = file.entries()                                                              
-    while(e.hasMoreElements()){
+    Enumeration <? extends ZipEntry> e = file.entries()                                                              
+    while (e.hasMoreElements()) {
         ZipEntry entry = e.nextElement()
         def entryName = entry.getName()
-        if ((entryName =~ project)){  
-            schedule = entryName.substring(entryName.lastIndexOf(project) + 9,entryName.lastIndexOf(project) + 20)     // parsing to get all the project names
-            if (schedule!=-1){ProjectList.add(schedule)}      
+        if (entryName =~ project) {  
+            schedule = entryName.substring(entryName.lastIndexOf(project) + 9, entryName.lastIndexOf(project) + 20)       // parsing to get all the project names
+            if (schedule != -1){
+				ProjectList.add(schedule)
+			}      
         } 
-        if ((entryName =~ branch)){  
-            schedule = entryName.substring(entryName.lastIndexOf(branch) + 9,entry.name.indexOf("_v") + 2)             // parsing to get all the branch names 
-            if (schedule!=-1){
-                version = entryName.substring(entry.name.indexOf("_v")+ 1,entry.name.indexOf("_r") + 3)
-                BranchList.add(schedule.replace("_v",""))           
+        if (entryName =~ branch) {  
+            schedule = entryName.substring(entryName.lastIndexOf(branch) + 9, entry.name.indexOf("_v") + 2)            	  // parsing to get all the branch names 
+            if (schedule != -1) {
+                version = entryName.substring (entry.name.indexOf("_v")+ 1, entry.name.indexOf("_r") + 3)
+                BranchList.add(schedule.replace ( "_v", "" ))           
             }      
         }  
     }
                                                                                                                     
-    def uniqueBranchList = BranchList.unique()                                                                         // leave unique branch names
-    def uniqueProjectList = ProjectList.unique()                                                                       // leave unique project names
+    def uniqueBranchList = BranchList.unique()                                                                         	  // leave unique branch names
+    def uniqueProjectList = ProjectList.unique()                                                                          // leave unique project names
         
         
     //reading configuration file   
          
     def BranchMap = [:]
     String delims = "[|]"
-    File conf = new File(conf_path)
-    conf.eachLine{line, lineNumber->
+    File conf = new File (conf_path)
+    conf.eachLine {line, lineNumber->
         if (lineNumber == 1) {           
-            exclude = line                                                                                              // set the exclude to have common exclude for all the branches
-            exclude_temp = line                                                                                         // store common exclude to reset exclude
+            exclude = line                                                                                                // set the exclude to have common exclude for all the branches
+            exclude_temp = line                                                                                           // store common exclude to reset exclude
         }
-        else if (lineNumber > 2) {emptyList.add(Arrays.toString(line.split(delims)))}                                   // read the rest of the file
+        else if (lineNumber > 2) {
+			emptyList.add(Arrays.toString(line.split(delims)))													  	  	  // read the rest of the file
+		}                                   
     }
     
     
     
-    uniqueProjectList.each{t->                                                                                          // loop through unique project names
-        new File(t).mkdirs()                                                                                            // create separate directory for each project name
+    uniqueProjectList.each{t->                                                                                            // loop through unique project names
+        new File(t).mkdirs()                                                                                              // create separate directory for each project name
         def temp = t - t.substring(t.lastIndexOf('_'), t.length())
-        uniqueBranchList.each{i->                                                                                       // loop through unique branch names
-            emptyList.eachWithIndex{p,c ->                                                                              // parse the list and add it to a map
-                BranchMap.put('project',(p.substring(0, p.indexOf(',')+1).replace(",","")).replace("[",""))             // getting a project name
-                p=p-p.substring(0, p.indexOf(',')+1)
-                BranchMap.branch=(p.substring(0, p.indexOf(',')+1).replace(",","")).replace(" ","")                     // getting a branch name
-                p=p-p.substring(0, p.indexOf(',')+1)
-                BranchMap.exclude=(p.substring(0, p.indexOf(',')+1).replace(",","")).replace(" ","")                    // getting the name of the elements to exclude+
-                p=p-p.substring(0, p.indexOf(',')+1)
-                BranchMap.include = (p.substring(p.indexOf(', ')+2,p.indexOf(']')+1).replace(",","")).replace("]","")
-                list[c]=BranchMap                                                                                       // add a map to a list
-                destFile =dir+ t+"\\"+i+".zip"                                                                          // create the name of the destination file
-                include ="**/"+t+"\\Branches\\"+i+"_v"+"*/"+"**"                                                        // concatinate the folder name that needs to be included
-                if((list[c].project==temp)&&(i==list[c].branch)){                                                   	// check if there is configuration for that branch and that project in the properties file
-                    exclude=exclude+",**/"+list[c].exclude+"/"+"**"                                                     // add the values that you want to exclude if the previos condition is true   
-                    //include=include+",**/"+list[c].include+"/"+"**"
-                    }
-                ant.zip(destfile: destFile){zipfileset(src:path,excludes:exclude,includes:include)}                     // create zips
-                exclude=exclude_temp                                                                                    // reset exclude to have just common exclude
+        uniqueBranchList.each{ i->                                                                                        // loop through unique branch names
+            emptyList.eachWithIndex{ p, c ->                                                                              // parse the list and add it to a map
+                BranchMap.put('project',(p.substring(0, p.indexOf(',')+1).replace(",","")).replace("[",""))               // getting a project name
+                p = p - p.substring(0, p.indexOf(',')+1)
+                BranchMap.branch = (p.substring(0, p.indexOf(',')+1).replace(",","")).replace(" ","")                     // getting a branch name
+                p = p - p.substring(0, p.indexOf(',')+1)
+                BranchMap.exclude = (p.substring(0, p.indexOf(',')+1).replace(",","")).replace(" ","")                    // getting the name of the elements to exclude+
+                p = p - p.substring(0, p.indexOf(',')+1)
+                BranchMap.include = (p.substring (p.indexOf(', ')+2,p.indexOf(']')+1).replace(",","")).replace("]","")
+                list[c] = BranchMap                                                                                       // add a map to a list
+                destFile = dir + t + "\\" +i + ".zip"                                                                     // create the name of the destination file
+                include = "**/" + t + "\\Branches\\" + i + "_v" + "*/" + "**"                                             // concatinate the folder name that needs to be included
+                if ((list[c].project == temp)&&(i == list[c].branch)) {                                                     // check if there is configuration for that branch and that project in the properties file
+                    exclude = exclude + ",**/" + (list[c].exclude).replace(",","/**,**/") + "/" + "**"                                           // add the values that you want to exclude if the previos condition is true   
+                    //include = include+",**/"+list[c].include+"/"+"**"
+                }
+                ant.zip ( destfile: destFile ) {
+					zipfileset (src:path, excludes:exclude, includes:include)											  // create zips
+				}                     									
+                exclude = exclude_temp                                                                                    // reset exclude to have just common exclude
             }       
        }              
     }
